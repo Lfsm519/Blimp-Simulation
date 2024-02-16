83f9255776708f8e67d12518f22518cb64bfad46
Yojan Gautam <ygautam2@gmu.edu>
2024-02-16 16:01:08 -0500

    added the blimp

--- main.py
+++ main.py
@@ -6,23 +6,26 @@ import numpy as np
 import cv2
 
 
-m = mujoco.MjModel.from_xml_path('test.xml')
+m = mujoco.MjModel.from_xml_path('sano.xml')
 d = mujoco.MjData(m)
 
 camera_name = 'blimp_camera'
 
 # Main simulation loop
-while True:
-    # Step the simulation
-    mujoco.mj_step(m, d)
+with mujoco.viewer.launch_passive(m, d) as viewer:
+    start = time.time()
+    while viewer.is_running() and time.time() - start < 30:
+        step_start = time.time()
 
-    # Get the camera observation
-    # camera_observation = d.render(
-    #     width=640, height=480, camera_name=camera_name)
-    #
-    # # Convert the observation to a NumPy array
-    # image_array = np.asarray(camera_observation)
-    #
-    # # Display or process the image as needed
-    # cv2.imshow('Camera Feed', image_array)
-    # cv2.waitKey(1)  # Adjust the delay as needed
+
+
+        with viewer.lock():
+            viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(d.time % 2)
+
+    # Pick up changes to the physics state, apply perturbations, update options from GUI.
+        viewer.sync()
+
+    # Rudimentary time keeping, will drift relative to wall clock.
+        time_until_next_step = m.opt.timestep - (time.time() - step_start)
+        if time_until_next_step > 0:
+            time.sleep(time_until_next_step)
